42bac2e19e55040c57ab2d5f5e1c4e65f4f7645f
yuliyago <yuuliya02@proton.me>
2023-08-16 14:27:39 +0300

    Maven task was done AGAIN.

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,21 +3,27 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try{
-                double numb = Double.parseDouble(str);
-                if(numb<=0) return false;
+            if (str.contains(" ")) return false;
+            double numb = Double.parseDouble(str);
+            if(numb<=0) return false;
         }
         catch(Exception e){
             return false;
         }
         return true;
+//        return org.apache.commons.lang3.StringUtils.isNumeric(str);
     }
 
     public static void main(String[] args) {
         System.out.println(isPositiveNumber(""));
         System.out.println(isPositiveNumber("-6"));
         System.out.println(isPositiveNumber("0"));
-        System.out.println(isPositiveNumber("23"));
+        System.out.println(isPositiveNumber("3"));
         System.out.println(isPositiveNumber("12.2"));
         System.out.println(isPositiveNumber("-9.5"));
+        System.out.println(isPositiveNumber("."));
+        System.out.println(isPositiveNumber("0.00"));
+        System.out.println(isPositiveNumber("  100.1"));
+        System.out.println(isPositiveNumber(null));
     }
 }
