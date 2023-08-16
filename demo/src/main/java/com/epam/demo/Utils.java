d6f6383f56bffa48fce418776739508e5afb8cc0
yuliyago <yuuliya02@proton.me>
2023-08-16 12:02:34 +0300

    Maven task is updated.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,13 +1,25 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String arg: args) {
-            double numb = Double.parseDouble(arg);
-            if(numb<=0) return false;
+        try{
+            for (String arg: args) {
+                double numb = Double.parseDouble(arg);
+                if(numb<=0) return false;
+            }
+        }
+      catch(Exception e){
+             return false;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        List<String> lst = new ArrayList<>();
+        lst.add(null);
+        System.out.println(isAllPositiveNumbers(lst));
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,20 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-       return org.apache.commons.lang3.StringUtils.isNumeric(str);
+        if(!str.equals("")){
+            String substr=str.split("\\.")[0];
+            if (Integer.parseInt(substr)==0) return false;
+            return org.apache.commons.lang3.StringUtils.isNumeric(substr);
+        }
+      return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber(""));
+        System.out.println(isPositiveNumber("-6"));
+        System.out.println(isPositiveNumber("0"));
+        System.out.println(isPositiveNumber("23"));
+        System.out.println(isPositiveNumber("12.2"));
+        System.out.println(isPositiveNumber("-9.5"));
     }
 }
